83f1c4ccf480405ba5df41f5a74cef2c42e41146
Edgar Rincón Morán <edgarrinconmoran@gmail.com>
2021-08-19 00:18:17 -0500

    hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -26,17 +26,16 @@ const Header = () => {
       <Container>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
-            {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
+            {title || "Hi, I'm"} <span className="text-color-main">{name || 'Edgar'}</span>
             <br />
-            {subtitle || "I'm the Unknown Developer."}
+            {subtitle || 'I make Websites'}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
           <p className="hero-cta">
             <span className="cta-btn cta-btn--hero">
               <Link to="about" smooth duration={1000}>
-                {cta || 'Know more'}
+                {cta || 'My Projects'}
               </Link>
             </span>
           </p>
